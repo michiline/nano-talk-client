c761b31342f1fcfa86c2d0291a3cb300acbfcff8
unknown <toma.simunic@fer.hr>
2020-04-28 21:31:59 +0200

    importing variables file

--- app/firebase.js
+++ app/firebase.js
@@ -2,23 +2,13 @@ import React from 'react'
 import * as firebase from 'firebase'
 import { Text, View, TextInput, Button, StyleSheet, TouchableOpacity, Platform } from 'react-native'
 import { FirebaseRecaptchaVerifierModal } from 'expo-firebase-recaptcha'
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyCWTkjuWiL4BlgUVjVKJ1axAmpIH1f3ViY',
-  authDomain: 'nano-talk-a1438.firebaseapp.com',
-  databaseURL: 'https://nano-talk-a1438.firebaseio.com',
-  projectId: 'nano-talk-a1438',
-  storageBucket: 'nano-talk-a1438.appspot.com',
-  messagingSenderId: '638790318520',
-  appId: '1:638790318520:web:f1cf605bd4467a9ae7f74d',
-  measurementId: 'G-0B83KM99RB'
-}
+import firebaseConfig from './variables'
 
 export const initFirebase = () => {
-  firebase.initializeApp(firebaseConfig)
+  firebase.initializeApp(firebaseConfig)  
 }
 
-export const firebasePhoneVerification = () => {
+export const firebasePhoneVerification = () => {  
   const recaptchaVerifier = React.useRef(null)
   const [phoneNumber, setPhoneNumber] = React.useState()
   const [verificationId, setVerificationId] = React.useState()
---
+++ app/variables.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCWTkjuWiL4BlgUVjVKJ1axAmpIH1f3ViY',
+  authDomain: 'nano-talk-a1438.firebaseapp.com',
+  databaseURL: 'https://nano-talk-a1438.firebaseio.com',
+  projectId: 'nano-talk-a1438',
+  storageBucket: 'nano-talk-a1438.appspot.com',
+  messagingSenderId: '638790318520',
+  appId: '1:638790318520:web:f1cf605bd4467a9ae7f74d',
+  measurementId: 'G-0B83KM99RB'
+}
+export {firebaseConfig}
+
